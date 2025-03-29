cc856ef571f86cae59677ab0b2413119cc9d46ce
Nelayn Abbas <nelaynabbas5@gmail.com>
2025-03-29 08:02:51 +0500

    offer management done

--- backend/app.py
+++ backend/app.py
@@ -170,6 +170,33 @@ def delete_items(item_id: dict):
             return {"message":"Items deteleted", "item":delete_items}
     raise HTTPException(status_code=404, detail="Item not found")
 
+@app.get("/offers")
+def get_offers():
+    return offers_db
+
+@app.post("/offers-management")
+def manage_offers(offer: Offer):
+    if offer.id:
+        for i, existing_offer in enumerate(offers_db):
+            if existing_offer["id"] == offer.id:
+                offer_dict = offer.dict()
+                offers_db = offer_dict
+                return offer_dict
+        raise HTTPException(status_code=404, detail="Offer not found")
+    else:
+        offer.id = str(uuid.uuid4())
+        offer_dict = offer.dict()
+        offers_db.append(offer_dict)
+        return offer_dict
+    
+@app.delete("/offers-management")
+def delete_offer(offer_id: dict):
+    for i,offer in enumerate(offers_db):
+        if offer["id"] == offer_id["id"]:
+            delete_offer = offers_db.pop(i)
+            return {"message":"Offer deleted", "offer":delete_offer}
+    raise HTTPException(status_code=404, detail="Offer not found")
+
 
 @app.get("/items")
 def get_items():
